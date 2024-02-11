89226c9e3cc2b24a8009fcd65afaccb6dd43f004
borendev <157404740+BorenDev@users.noreply.github.com>
2024-02-11 13:04:40 -0700

    add FreeRTOS-Kernel as a submodule

--- demo/hello.cpp
+++ demo/hello.cpp
@@ -1,6 +1,15 @@
+#include <stdint.h>
 #include <stdio.h>
 
-int main() {
+#include "FreeRTOS.h"
+#include "task.h"
+
+void test() {
+  printf("tskKERNEL_VERSION_NUMBER: %s\n", tskKERNEL_VERSION_NUMBER);
+}
+
+int32_t main() {
   printf("Hello Willis\n");
+  test();
   return 0;
 }
